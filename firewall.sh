c1b419729254955bf9e3ac52176e6b288ff2a1da
alisalehiman <alisalehiman@users.noreply.github.com>
2019-09-01 17:05:50 +0430

    temporary remove if statement

--- firewall.sh
+++ firewall.sh
@@ -4,12 +4,6 @@ export RUNTIME_DATE="$(date +%F)"
 echo "Removing old ArvanCloud IPs from CSF ..."
 sed -i '/##start-arvan-ip/,/##end-arvan-ip/d' /etc/csf/csf.allow
 
-if [ $? -ne 0 ]
-then
-    echo "Removed old IPs sucessfully."
-else
-    echo "Couldn't remove old IPs from /etc/csf/csf.allow."
-fi
 
 echo "Downloading new IPs From ArvanCloud ..."
 wget -O /tmp/arvan_new_ips_$RUNTIME_DATE.txt "https://arvancloud.com/ips.txt"
@@ -22,28 +16,10 @@ do
   echo "tcp|in|d=80_443|s=$line" >> /etc/csf/csf.allow
 done < "$input"
 
-if [ $? -ne 0 ]
-then
-    echo "Added new IPs to csf allow file successfully."
-else
-    echo "Couldn't add new IPs to /etc/csf/csf.allow."
-fi
-
 echo "##end-arvan-ip" >> /etc/csf/csf.allow
+
+echo "Added new IPs to csf allow file successfully."
 
 echo "Restarting CSF ..."
 csf -r
-
-if [ $? -ne 0 ]
-then
-    echo "ArvanCloud IPs in CSF have been updated successfully. Done!"
-else
-    echo "Couldn't restart CSF, trying to start and stop it ..."
-    csf -f && csf -s
-    if [ $? -ne 0 ]
-      then
-      echo "Now good to go, ArvanCloud IPs in CSF have been updated successfully. Done!"
-    else
-      echo "Couldn't start CSF, please check out logs for error ..."
-    fi
-fi
+echo "Now good to go, ArvanCloud IPs in CSF have been updated successfully. Done!"
